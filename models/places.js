f1c538e70fccaa7769474e3eb2eb9487c59f666f
Alex Castaneda <alex@Alexs-MacBook-Pro.local>
2023-02-23 19:58:33 -0800

    new places can be added

---
+++ models/places.js
@@ -0,0 +1,16 @@
+
+
+
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: 'public/images/Thai-food.jpg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: 'public/images/bread.jpg'
+  }]
